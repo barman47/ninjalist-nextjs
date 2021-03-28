15478891aff3179c51cec1596491e2a974c1476f
barman47 <nomsouzoanya@yahoo.co.uk>
2021-03-28 22:33:48 +0100

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="about" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum dicta cumque, totam provident, doloribus suscipit expedita atque dolor, sint at delectus! Hic id distinctio et in soluta a accusamus sint?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum dicta cumque, totam provident, doloribus suscipit expedita atque dolor, sint at delectus! Hic id distinctio et in soluta a accusamus sint?</p>
+            </div>
+        </>
+    );
+};
+
+export default About;
